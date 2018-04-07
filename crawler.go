47c008c997bc492e2f1cefa44f60c49210bc8beb
Naoya Otsuka <otknoy@gmail.com>
2018-04-07 10:38:24 +0900

    Use goroutine

--- crawler.go
+++ crawler.go
@@ -1,52 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 
-	"github.com/go-redis/redis"
 	"github.com/otknoy/dmm-crawler/dmm"
 )
 
 func main() {
+	actresses := []string{}
+	stdin := bufio.NewScanner(os.Stdin)
+	for stdin.Scan() {
+		actress := stdin.Text()
+		actresses = append(actresses, actress)
+	}
+
 	apiid := os.Getenv("DMM_API_ID")
 	affid := os.Getenv("DMM_AFFILIATE_ID")
-
 	itemSearchClient := dmm.NewItemSearchClientImpl(apiid, affid)
-	redisClient := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
 
-	keyword := "つぼみ"
-
-	res, err := itemSearchClient.Search(keyword)
-	if err != nil {
-		log.Print(err)
-	}
-
-	items := res.Result.Items
-	for _, item := range items {
-		bytes, err := json.Marshal(item)
-		if err != nil {
-			log.Print(err)
+	responses := make(chan dmm.ItemResponse, 10)
+	go func(keywords []string) {
+		for _, keyword := range actresses {
+			res, _ := itemSearchClient.Search(keyword)
+			responses <- res
 		}
+		close(responses)
+	}(actresses)
 
-		err = redisClient.Publish("item_feed", bytes).Err()
-		if err != nil {
-			panic(err)
+	for itemResponse := range responses {
+		keyword := itemResponse.Request.Parameters.Keyword
+		log.Println(keyword)
+		for i, item := range itemResponse.Result.Items {
+			bytes, _ := json.Marshal(item)
+
+			outputDir := "/mnt/temp/dmm/"
+			filename := outputDir + fmt.Sprintf("%04d_%s.json", i, keyword)
+			// log.Printf("success to save file: %s", filename)
+			err := ioutil.WriteFile(filename, bytes, os.ModePerm)
+			if err != nil {
+				log.Fatalf("failed to write file: %s", filename)
+			}
 		}
-
-		// filename := "/mnt/temp/dmm/" + fmt.Sprintf("%04d_%s.json", i, keyword)
-		// log.Printf("save file: %s", filename)
-		// save(filename, bytes)
-	}
-}
-
-func save(filename string, bytes []byte) {
-	err := ioutil.WriteFile(filename, bytes, os.ModePerm)
-	if err != nil {
-		log.Fatalf("failed to write file: %s", filename)
 	}
 }
