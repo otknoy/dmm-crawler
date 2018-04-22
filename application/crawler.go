6cdd88791a8fb34c9542948d7541c054df3d2010
Naoya Otsuka <otknoy@gmail.com>
2018-04-23 02:37:20 +0900

    Refactoring

--- application/crawler.go
+++ application/crawler.go
@@ -27,10 +27,11 @@ func (c *Crawler) Crawl() {
 	items := make(chan model.Item)
 	go c.process(responses, items)
 
-	// c.save(items)
-
 	solr := infrastructure.NewSolrRepository()
 	for item := range items {
+		filename := fmt.Sprintf("/mnt/temp/dmm/%s.json", item.ID)
+		save(filename, item)
+
 		err := solr.Add(item)
 		if err != nil {
 			log.Fatalln(err)
@@ -57,16 +58,14 @@ func (c *Crawler) process(in chan []model.Item, out chan model.Item) {
 	close(out)
 }
 
-func (c *Crawler) save(items chan model.Item) {
-	for item := range items {
-		bytes, _ := json.Marshal(item)
-
-		outputDir := "/mnt/temp/dmm/"
-		filename := outputDir + fmt.Sprintf("%s.json", item.ID)
-		err := ioutil.WriteFile(filename, bytes, os.ModePerm)
-		if err != nil {
-			log.Fatalf("failed to write file: %s", filename)
-		}
-		// log.Printf("success to save file: %s", filename)
+func save(filename string, o interface{}) error {
+	bytes, _ := json.Marshal(o)
+	err := ioutil.WriteFile(filename, bytes, os.ModePerm)
+	if err != nil {
+		log.Fatalf("failed to write file: %s", filename)
+		return err
 	}
+	log.Printf("success to save file: %s", filename)
+
+	return nil
 }
