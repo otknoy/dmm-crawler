6190f60b8ffea0440bb748e679eb20a102e93808
Naoya Otsuka <otknoy@gmail.com>
2018-04-23 00:51:56 +0900

    minor change

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -5,3 +5,6 @@ import "github.com/otknoy/dmm-crawler/model"
 type ItemSearcher interface {
 	Search(keyword string, hits int, offset int) (model.ItemResponse, error)
 }
+
+type ItemSaver struct {
+}
