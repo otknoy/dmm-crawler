2f9f3b7df6b292250822c6def1bce0c290285c5c
Naoya Otsuka <otknoy@gmail.com>
2018-04-22 23:40:27 +0900

    Fix

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	"os"
 
 	"github.com/otknoy/dmm-crawler/application"
-	"github.com/otknoy/dmm-crawler/domain/repository"
 	"github.com/otknoy/dmm-crawler/domain/service"
+	"github.com/otknoy/dmm-crawler/infrastructure"
 )
 
 func main() {
 	apiid := os.Getenv("DMM_API_ID")
 	affid := os.Getenv("DMM_AFFILIATE_ID")
-	dmmItemRepository := repository.NewDmmItemRepository(apiid, affid)
+	dmmItemRepository := infrastructure.NewDmmItemRepository(apiid, affid)
 
 	itemService := service.NewItemService(dmmItemRepository)
 
