9c5e3ed7a24b085dae010dd2bb3c016d68f635ed
otknoy <otknoy@gmail.com>
2018-05-04 01:58:39 +0900

    minor change

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 	"time"
 
-	"github.com/garyburd/redigo/redis"
 	"github.com/otknoy/dmm-crawler/application"
 	"github.com/otknoy/dmm-crawler/infrastructure"
 	"github.com/otknoy/dmm-crawler/model"
@@ -41,30 +38,5 @@ func main() {
 
 		count++
 		log.Println(count)
-	}
-}
-
-func subscriber() {
-	log.Println("Subscriber")
-
-	conn, _ := redis.Dial("tcp", "localhost:6379")
-	psc := redis.PubSubConn{Conn: conn}
-	psc.Subscribe("dmm-items")
-	for {
-		switch v := psc.Receive().(type) {
-		case redis.Message:
-			item := &model.DmmItem{}
-			if err := json.Unmarshal(v.Data, item); err != nil {
-				fmt.Println(err)
-			}
-
-			// fmt.Println(string(v.Data))
-			fmt.Println(item.Title)
-			// fmt.Printf("%s: message: %s\n", v.Channel, v.Data)
-		case redis.Subscription:
-			fmt.Printf("%s: %s %d\n", v.Channel, v.Kind, v.Count)
-		case error:
-			fmt.Println("error")
-		}
 	}
 }
