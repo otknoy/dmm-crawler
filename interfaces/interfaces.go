81c2b059d2823d91babb89475de28fbde715d50c
otknoy <otknoy@gmail.com>
2018-05-07 00:51:09 +0900

    Remove unnecessary interface

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -5,15 +5,3 @@ import "github.com/otknoy/dmm-crawler/model"
 type ItemSearcher interface {
 	Search(keyword string, hits int, offset int) (model.ItemResponse, error)
 }
-
-type ItemSaver interface {
-	Save(key string, data interface{}) error
-}
-
-type ItemRepository interface {
-	Insert(model.DmmItem) error
-}
-
-type ItemPublisher interface {
-	Publish(model.DmmItem) error
-}
